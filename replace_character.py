acbfd2ede946add76db3a3b47e76b861616f08f1
Kavin.S <senthilkavi1972@gmail.com>
2025-07-25 20:59:46 +0530

    Create replace_character.py

---
+++ replace_character.py
@@ -0,0 +1,9 @@
+def mutate_string(string, position, character):
+    l=list(string)
+    l[position]=character
+    return''.join(1)
+if __name__ == '__main__':
+    s = input()
+    i, c = input().split()
+    s_new = mutate_string(s, int(i), c)
+    print(s_new)
