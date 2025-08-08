529a69f1e897084e074e125f37d11f10a8aa97d4
Kavin.S <senthilkavi1972@gmail.com>
2025-08-08 11:18:56 +0530

    Update remove_element.py

--- remove_element.py
+++ remove_element.py
@@ -9,3 +9,8 @@ def remove_element(nums:List[int],val:int):->int:
 
 
 #exapmle usage
+nums=[3,4,5,6,78,9,0,1,2,3,4,5,6]
+val=3
+rc=remove_element(nums,val)
+print("New length:", new_length)
+print("Modified list:", nums[:new_length])
