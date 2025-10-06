0c5d251f47a8ab433435b8687a0dc744384d5617
Kavin.S <senthilkavi1972@gmail.com>
2025-10-06 18:50:12 +0530

    Update Reverse_bit.java

--- Reverse_bit.java
+++ Reverse_bit.java
@@ -8,9 +8,25 @@ public class Main{
        result<<=1;
        result |=(n&1);
        n>>=1;}
+      sc.close();
      System.out.println(result);}
 }
-    
+
+
+//Another method
+import java.util.Scanner;
+public class Main{
+   public static void main(String[] args){
+      Scanner sc=new Scanner(System.in);
+      int n=sc.nextInt();
+      String binary=String formate("%32s"+Integer.toBinaryString(n);
+      String res=new StringBuilder(binary).reverse().toString();
+      int num=Integer.parseInt(res);
+      System.out.println(num);
+      sc.close(); 
+   }
+}
+   
  
       
   
