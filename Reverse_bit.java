82d3ac1098fb5bb60c894fba3f1949405b5e4f12
Kavin.S <senthilkavi1972@gmail.com>
2025-10-06 18:39:51 +0530

    Update Reverse_bit.java

--- Reverse_bit.java
+++ Reverse_bit.java
@@ -1,1 +1,16 @@
-
+import java.util.Scanner;
+public class main{
+   public Static void main(String[] args){
+     int result=0;
+     Scanner sc=new Scanner(System.in);
+     int n=sc.nextInt();
+     for(int i=0;i<32,i++){
+       result<<=1;
+       result |=(n&1);
+       n>>=1;}
+     return result;}
+}
+    
+ 
+      
+  
