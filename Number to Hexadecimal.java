b29a9eed456e2e0ddedbe6454f9c38d28056c254
Kavin.S <senthilkavi1972@gmail.com>
2025-10-09 22:12:29 +0530

    Update Number to Hexadecimal.java

--- Number to Hexadecimal.java
+++ Number to Hexadecimal.java
@@ -1,1 +1,13 @@
+public class HexExample {
+    public static void main(String[] args) {
+        HexExample obj = new HexExample();
+        String hexValue = obj.toHex(255);
+        System.out.println("Hex value: " + hexValue);
+    }
+
+    public String toHex(int num) {
+        String str = Integer.toHexString(num);
+        return str;
+    }
+}
 
