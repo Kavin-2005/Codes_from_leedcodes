f5dc8efe700b54a61b109aee7b7cc294cde50b65
Kavin.S <senthilkavi1972@gmail.com>
2025-10-18 19:13:08 +0530

    Update Valid Anagram(242).java

--- easy/Valid Anagram(242).java
+++ easy/Valid Anagram(242).java
@@ -1,1 +1,15 @@
-
+import java.util.Arrays;
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        char []arr=s.toCharArray();
+        char []arr2=t.toCharArray();
+        Arrays.sort(arr);
+        Arrays.sort(arr2);
+        String str=new String(arr);
+        String str1=new String(arr2);
+        if(str.equals(str1)){
+            return true;
+        }
+        else return false; 
+    }
+}
