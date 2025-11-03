36636a312da06c01549aa474f1ced106af466644
Kavin.S <senthilkavi1972@gmail.com>
2025-11-03 20:52:47 +0530

    Update CointainDuplicate217.java

--- easy/CointainDuplicate217.java
+++ easy/CointainDuplicate217.java
@@ -8,4 +8,22 @@ class Solution {
         }return false;     
     }
 }
+another method
+  class Solution {
+    public boolean containsDuplicate(int[] nums) {
+        int count = 0;
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = i + 1; j < nums.length; j++) {
+                if (nums[i] == nums[j]) {
+                    count++;
+                }
+            }
+        }
+        if (count > 0) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
 
