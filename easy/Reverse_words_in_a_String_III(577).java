31f3e54d56562e0b7d59610111af082b304c4d05
Kavin.S <senthilkavi1972@gmail.com>
2025-11-10 07:15:56 +0530

    Update Reverse_words_in_a_String_III(577).java

--- easy/Reverse_words_in_a_String_III(577).java
+++ easy/Reverse_words_in_a_String_III(577).java
@@ -1,1 +1,15 @@
-
+class Solution {
+    public String reverseWords(String s) {
+        String[] arr=s.split(" ");
+        StringBuilder sb=new StringBuilder();
+        for(int i=0;i<arr.length;i++){
+            StringBuilder sbc=new StringBuilder(arr[i]);
+            sbc.reverse();
+            sb.append(sbc);
+        if(i<arr.length-1)
+        sb.append(" ");}
+return sb.toString();
+        
+    }
+}
+//my own method with chatgpt (asking help like logic and concept//
