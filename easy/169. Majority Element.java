74a40cd96589727a83a3ca80bb361e2065f1319c
Kavin.S <senthilkavi1972@gmail.com>
2025-11-11 07:15:33 +0530

    Update 169. Majority Element.java

--- easy/169. Majority Element.java
+++ easy/169. Majority Element.java
@@ -1,1 +1,19 @@
+class Solution {
+    public int majorityElement(int[] nums) {
+        int flag=1;
+        Arrays.sort(nums);
+        int max_count =1;
+        int number=nums[0];
+        for(int i=0;i<nums.length;i++){
+            if(arr[i]==arr[i+1]){
+                flag++;
+            }
+            elae{
+                max_count=flag;
+                number=nums[i];
+            }
 
+        }
+        
+    }
+}
