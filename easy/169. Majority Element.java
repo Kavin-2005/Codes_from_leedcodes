7c88c21cee67401ec9b29ee1557c0db41d1fd680
Kavin.S <senthilkavi1972@gmail.com>
2025-11-11 07:16:12 +0530

    Implement majority element finder using HashMap
    Added a HashMap implementation to find the majority element in an array.

--- easy/169. Majority Element.java
+++ easy/169. Majority Element.java
@@ -17,3 +17,32 @@ class Solution {
         
     }
 }
+using haph map
+    import java.util.HashMap;
+import java.util.Map;
+
+public class Main {
+    public static void main(String[] args) {
+        int[] arr = {2, 3, 5, 3, 2, 3, 2, 5, 3};
+
+        HashMap<Integer, Integer> map = new HashMap<>();
+
+        for (int num : arr) {
+            map.put(num, map.getOrDefault(num, 0) + 1);
+        }
+
+        int maxElement = -1;
+        int maxCount = 0;
+
+        for (Map.Entry<Integer, Integer> entry : map.entrySet()) {
+            if (entry.getValue() > maxCount) {
+                maxCount = entry.getValue();
+                maxElement = entry.getKey();
+            }
+        }
+
+        System.out.println("Number repeated maximum times: " + maxElement);
+        System.out.println("Frequency: " + maxCount);
+    }
+}
+
