ad9e9a9ed8d1ceb0d218fc9e8360523fe683a2e3
Kavin.S <senthilkavi1972@gmail.com>
2026-01-08 21:58:42 +0530

    Create 832.java

---
+++ easy/832.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int[][] flipAndInvertImage(int[][] image) {
+        for(int i=0;i<image.length;i++){
+            for(int j=0;j<(image[0].length)/2;j++){
+                int temp=image[i][j];
+                image[i][j]=image[i][image[0].length-1-j];
+                image[i][image[0].length-1-j]=temp;
+            }
+        }
+        for(int i=0;i<image.length;i++){
+            for(int j=0;j<image[0].length;j++){
+                image[i][j]=image[i][j]==0?1:0;
+            }
+        }
+        return image;
+        
+    }
+}
