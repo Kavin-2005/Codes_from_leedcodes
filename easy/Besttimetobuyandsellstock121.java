19d05fd1b9593dc1f9ddc15e5d4ac3089e9a45ab
Kavin.S <senthilkavi1972@gmail.com>
2026-01-25 09:45:01 +0530

    Update Besttimetobuyandsellstock121.java

--- easy/Besttimetobuyandsellstock121.java
+++ easy/Besttimetobuyandsellstock121.java
@@ -12,3 +12,18 @@ class Solution {
         return ans;
     }
 }
+class Solution {
+    public int maxProfit(int[] prices) {
+        int ans=0;
+        int m=prices[0];
+        int temp=0;
+        for(int i=0;i<prices.length;i++){
+           if(prices[i]<m)
+           m=prices[i];
+            temp=prices[i]-m;
+            if(ans<temp)
+            ans=temp;
+        }
+        return ans;
+    }
+}
