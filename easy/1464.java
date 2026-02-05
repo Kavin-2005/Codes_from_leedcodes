9ccb22e434734c9a030f44fa3eea427b9b31126f
Kavin.S <senthilkavi1972@gmail.com>
2026-02-05 22:07:00 +0530

    Create 1464.java

---
+++ easy/1464.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int maxProduct(int[] nums) {
+        int max=0;
+        Arrays.sort(nums);
+        max=(nums[nums.length-1]-1)*(nums[nums.length-2]-1);
+        return max;
+    }
+}
+class Solution {
+    public int maxProduct(int[] nums) {
+        int max=0;
+        for(int i=0;i<nums.length;i++){
+            for(int j=0;j<nums.length-1;j++){
+                if(i==j){
+                    j=j+1;
+                }
+                int secoundsum=((nums[i]-1)*(nums[j]-1));
+                if(max<secoundsum){
+                    max=secoundsum;
+                }
+
+            }
+        }
+        return max;
+    }
+}
