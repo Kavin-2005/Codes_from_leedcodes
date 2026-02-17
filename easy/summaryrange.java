124e7f01bca325d3ce6732e86e89b68019115370
Kavin.S <senthilkavi1972@gmail.com>
2026-02-17 23:01:23 +0530

    Create summaryrange.java

---
+++ easy/summaryrange.java
@@ -0,0 +1,21 @@
+class Solution {
+    public List<String> summaryRanges(int[] nums) {
+        List<String> summary=new ArrayList();
+         if(nums.length==0) return summary;
+         int start=nums[0];
+        for(int i=1;i<=nums.length;i++){
+            if(i==nums.length||nums[i]!=nums[i-1]+1){
+                if(start==nums[i-1]){
+                    summary.add(String.valueOf(start));
+                }
+                else{
+                    summary.add(start+"->"+nums[i-1]);
+                }
+                if(i<nums.length){
+                    start=nums[i];
+                }
+            }
+        }
+        return summary;
+    }
+}
