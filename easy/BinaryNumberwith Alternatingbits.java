727242c3d6583ca059876303cfa16f8cc2610077
Kavin.S <senthilkavi1972@gmail.com>
2026-02-18 20:51:52 +0530

    Refactor hasAlternatingBits for clarity and efficiency
    Refactor hasAlternatingBits method to use character array and string directly for checking alternating bits.

--- easy/BinaryNumberwith Alternatingbits.java
+++ easy/BinaryNumberwith Alternatingbits.java
@@ -1,9 +1,10 @@
+//my methode 
 class Solution {
     public boolean hasAlternatingBits(int n) {
-        String str=Integer.toBinaryString(n);
-        char [] arr=str.toCharArray();
+        String str=Integer.toBinaryString(n);//int to binary string
+        char [] arr=str.toCharArray();//storing it in charArray
         for(int i=0;i<arr.length-1;i++){
-            if(arr[i]==arr[i+1]){
+            if(arr[i]==arr[i+1]){//checking first and its adjecent number
                 return false;
             }
         }
@@ -11,11 +12,12 @@ class Solution {
     }
 }
 or 
+    //alternaive methode oms 
   class Solution {
     public boolean hasAlternatingBits(int n) {
-        String str=Integer.toBinaryString(n);
+        String str=Integer.toBinaryString(n);//int to binary
         for(int i=0;i<str.length()-1;i++){
-            if(str.charAt(i)==str.charAt(i+1)){
+            if(str.charAt(i)==str.charAt(i+1)){//checks its character to near one 
                 return false;
             }
         }
