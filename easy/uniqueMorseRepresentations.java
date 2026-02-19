b4b5bbd9dcc71209d7eff43bd0fbc8593d20e5c2
Kavin.S <senthilkavi1972@gmail.com>
2026-02-19 21:51:43 +0530

    Update uniqueMorseRepresentations.java

--- easy/uniqueMorseRepresentations.java
+++ easy/uniqueMorseRepresentations.java
@@ -12,3 +12,33 @@ class Solution {
         return hs.size(); 
     }
 }
+// secoud methode 
+import java.util.*;
+
+class Solution {
+
+    private static final String[] MORSE = {
+        ".-","-...","-.-.","-..",".","..-.","--.","....","..",".---",
+        "-.-",".-..","--","-.","---",".--.","--.-",".-.","...","-",
+        "..-","...-",".--","-..-","-.--","--.."
+    };
+
+    public int uniqueMorseRepresentations(String[] words) {
+
+        Set<String> set = new HashSet<>();
+
+        for (String word : words) {
+
+            StringBuilder sb = new StringBuilder();
+
+            for (int i = 0; i < word.length(); i++) {
+                sb.append(MORSE[word.charAt(i) - 'a']);
+            }
+
+            set.add(sb.toString());
+        }
+
+        return set.size();
+    }
+}
+//third methode
