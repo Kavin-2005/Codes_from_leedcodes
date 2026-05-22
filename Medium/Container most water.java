b7ca073fbda3bfc00559ff36d40bea81b7b01495
Kavin.S <senthilkavi1972@gmail.com>
2026-05-23 00:02:11 +0530

    Update Container most water.java

--- Medium/Container most water.java
+++ Medium/Container most water.java
@@ -27,3 +27,25 @@ class Solution {
         return max;
     }
 }
+class Solution {
+    public int maxArea(int[] height) {
+
+        int max = 0;
+
+        for (int i = 0; i < height.length; i++) {
+
+            for (int j = i + 1; j < height.length; j++) {
+
+                int width = j - i;
+
+                int minHeight = Math.min(height[i], height[j]);
+
+                int area = width * minHeight;
+
+                max = Math.max(max, area);
+            }
+        }
+
+        return max;
+    }
+}
