61b30e1d38a3e422cb86e7bfdd33e4b1fd90aed7
Kavin.S <senthilkavi1972@gmail.com>
2026-05-23 23:48:19 +0530

    Create sortnumber without sort.java

---
+++ Medium/sortnumber without sort.java
@@ -0,0 +1,24 @@
+class Solution {
+    public void sortColors(int[] nums) {
+        int arr[]=nums;
+        for(int i=0;i<nums.length;i++){
+            for(int j=0;j<nums.length;j++){
+                if(nums[i]>nums[j]){
+                    int temp=arr[i];
+                    arr[i]=arr[j];
+                    arr[j]=temp;
+                }
+
+            }
+        }
+        int left=0,right=nums.length-1;
+        while(left<right){
+            int temp=arr[left];
+            arr[left]=arr[right];
+            arr[right]=temp;
+            left++;right--;
+
+        }
+        
+    }
+}
