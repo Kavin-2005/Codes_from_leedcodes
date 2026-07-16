74ad7a759ae4118dd96d00fae62769b8da958348
Kavin.S <senthilkavi1972@gmail.com>
2026-07-16 20:05:32 +0530

    Update maximum-number-of-coins-you-can-get.java

--- Medium/maximum-number-of-coins-you-can-get.java
+++ Medium/maximum-number-of-coins-you-can-get.java
@@ -1,1 +1,15 @@
+// slite chages made by chat gpt
+class Solution {
+    public int maxCoins(int[] piles) {
+        Arrays.sort(piles);
 
+        int n = piles.length;
+        int ans = 0;
+
+        for (int i = n - 2, count = 0; count < n / 3; i -= 2, count++) {
+            ans += piles[i];
+        }
+
+        return ans;
+    }
+}
