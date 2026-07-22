28ce9fab72987d345f7a9b4e0fb967fc6bec8092
Kavin.S <senthilkavi1972@gmail.com>
2026-07-22 11:43:54 +0530

    Update find_Common_characters.java

--- easy/find_Common_characters.java
+++ easy/find_Common_characters.java
@@ -1,3 +1,4 @@
+//my own solution
 class Solution {
     public List<String> commonChars(String[] words) {
 
@@ -39,6 +40,47 @@ class Solution {
                 ans.add(String.valueOf(ch));
             }
         }
+
+        //optimized solution
+        
+        class Solution {
+    public List<String> commonChars(String[] words) {
+
+        List<String> ans = new ArrayList<>();
+
+        // Frequency of first word
+        int[] minFreq = new int[26];
+
+        for (char ch : words[0].toCharArray()) {
+            minFreq[ch - 'a']++;
+        }
+
+        // Compare with remaining words
+        for (int i = 1; i < words.length; i++) {
+
+            int[] freq = new int[26];
+
+            for (char ch : words[i].toCharArray()) {
+                freq[ch - 'a']++;
+            }
+
+            // Keep the minimum frequency
+            for (int j = 0; j < 26; j++) {
+                minFreq[j] = Math.min(minFreq[j], freq[j]);
+            }
+        }
+
+        // Build the answer
+        for (int i = 0; i < 26; i++) {
+            while (minFreq[i] > 0) {
+                ans.add(String.valueOf((char)(i + 'a')));
+                minFreq[i]--;
+            }
+        }
+
+        return ans;
+    }
+}
 
         return ans;
     }
