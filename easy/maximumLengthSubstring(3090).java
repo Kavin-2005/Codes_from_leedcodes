c4a777d16972f5994c0a153af9e7c073edc59015
Kavin.S <senthilkavi1972@gmail.com>
2026-08-14 09:57:43 +0530

    Update maximumLengthSubstring(3090).java
    adding two new methode on today streak question

--- easy/maximumLengthSubstring(3090).java
+++ easy/maximumLengthSubstring(3090).java
@@ -1,1 +1,57 @@
+// Hash Map
+import java.util.HashMap;
 
+class Solution {
+    public int maximumLengthSubstring(String s) {
+
+        int maxLength = 0;
+
+        for (int i = 0; i < s.length(); i++) {
+
+            HashMap<Character, Integer> map = new HashMap<>();
+
+            for (int j = i; j < s.length(); j++) {
+
+                char ch = s.charAt(j);
+
+                map.put(ch, map.getOrDefault(ch, 0) + 1);
+
+                // Character occurs more than twice
+                if (map.get(ch) > 2) {
+                    break;
+                }
+
+                maxLength = Math.max(maxLength, j - i + 1);
+            }
+        }
+
+        return maxLength;
+    }
+}
+// brute force
+class Solution {
+    public int maximumLengthSubstring(String s) {
+        int maxLength = 0;
+
+        for (int i = 0; i < s.length(); i++) {
+
+            int[] count = new int[26];
+
+            for (int j = i; j < s.length(); j++) {
+
+                char ch = s.charAt(j);
+
+                count[ch - 'a']++;
+
+                // Character appears more than 2 times
+                if (count[ch - 'a'] > 2) {
+                    break;
+                }
+
+                maxLength = Math.max(maxLength, j - i + 1);
+            }
+        }
+
+        return maxLength;
+    }
+}
