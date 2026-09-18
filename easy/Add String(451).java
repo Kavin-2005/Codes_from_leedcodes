86cadb9904e6e3e0910980d35365a1dc43659a85
Kavin.S <senthilkavi1972@gmail.com>
2026-09-18 14:35:03 +0530

    Update and rename Add String.java(451) to Add String(451).java

---
+++ easy/Add String(451).java
@@ -0,0 +1,8 @@
+class Solution {
+    public String addStrings(String num1, String num2) {
+        int sum=0;
+        sum=Double.valueOf(num1).intValue()+Double.valueOf(num2).intValue();
+        return String.valueOf(sum);
+        
+    }
+}// owm methode for me nut it is not corretc 
